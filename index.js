9350219a13c7b44129a2ca70b0cfdf9beb2b36c2
Christian Loera <114829265+ArticFox04@users.noreply.github.com>
2023-04-11 13:46:29 -0600

    added questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your application.',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Installation intructions?'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How can your applicaiton be used?'
+  },
+  {
+    type: 'input',
+    name: 'contributions',
+    message: 'Provide credit to any contributers'
+  },
+  {
+    type: 'list',
+    message: 'Which license would you like to select?',
+    name: 'license',
+    choices: ['MIT License','Apache License 2.0','GNU General Public License v2.0', 'MIT', 'Boost Software License 1.0', 'None']
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'What is your github username?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
